dccc778562e23a70cc62a38c1cb67d3924605c6c
Eric Bui <bui@MacBook-Pro-14-M3-Max.local>
2025-04-08 20:57:02 -0700

    add solution for Contains Duplicate

---
+++ 01_Contains_Duplicate.ts
@@ -0,0 +1,48 @@
+// Ideal solution offering both O(n) time and memory complexity
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {boolean}
+   */
+  hasDuplicate(nums): boolean {
+    // Javascript Set creates a hash map from nums with only unique values
+    // We can then compare the length of the Set and the original nums array to determine if any duplicates exist
+    const seen: Set<Number> = new Set(nums);
+    return [...seen].length !== nums.length;
+  }
+}
+
+// Brute Force solution:
+class BruteForceSolution {
+  /**
+   * @param {number[]} nums
+   * @return {boolean}
+   */
+  hasDuplicate(nums) {
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] === nums[j]) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
+
+// Sorting Solution
+class SortingSolution {
+  /**
+   * @param {number[]} nums
+   * @return {boolean}
+   */
+  hasDuplicate(nums) {
+    nums.sort((a, b) => a - b);
+    for (let i = 1; i < nums.length; i++) {
+      if (nums[i] === nums[i - 1]) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
